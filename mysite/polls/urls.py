81ac7179ede9d2b01db05190f50b3479a6fb91a8
unknown <tiagosanabria03@gmail.com>
2024-08-22 16:47:04 -0500

    Se hacen unas vistas provisionales para mostrar con se pueden mostrar datos que el usuario ingrese en la URL en la pantalla principal de la pagina, se definen rutas en urls.py a nivel de App

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Ponemos la ruta que se necesita para entrar, nombre de la fucion en el modulo y nombre
+    #Ponemos la ruta que se necesita para entrar, nombre de la funcion en el modulo y nombre
     #Ahora solo queda definir la ruta a nivel de proyecto
+
     path("primera_vista/", views.index, name="index"),
+
+    #Se muestra como se definen los datos que ingresa el usuario por medio de la URL, en Views se muestra como hacer para que estos valores se vean en pantalla
+    # ex: /polls/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 
 #Creamos una vista simple en django, ahora tenemos que definirle una url
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hola bebe este es el index request")
+
+#Definimos las vistas para mostrar como podemos anexar datos que ingrese el usuario en la url en la pagina principal
+def detail(request, question_id):
+    return HttpResponse("Estas mirando la pregunta numero: %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Y Estas mirando los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Tu voto en la pregunta es de: %s." % question_id)
