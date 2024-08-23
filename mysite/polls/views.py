ca455a3bb6b33b3d1b07e4f2193421cc6a7c4374
unknown <tiagosanabria03@gmail.com>
2024-08-23 13:53:32 -0500

    Se Crea carptea templates junto con el archivo index.html tambien se crea nueva funcion index para que llame a la plantilla que acabamos de crear, se crea un nuevo registro de prueba en SQL

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 # Create your views here.
@@ -24,9 +26,23 @@ def vote(request, dato_usuario_nav):
 #Una vista puede hacer 2 cosas, o devolver un HttpResponse con el contenido HTML que queremos ver o hacer un excepcion con Http404
 
 #Comentamos la vista index y creamos una nueva, esta lo que hace es mostrarnos todos los objetos del modelo Questions odenado por fecha de publicacion separados por , 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+
+# def index(request):
+#     ultimas_preguntas_hechas = Question.objects.order_by("-pub_date")[:5]
+#     output = ", ".join([q.question_text for q in ultimas_preguntas_hechas])
+#     return HttpResponse(output)
 
 #Funciona correctamente pero no es lo correcto codificar el diseño de pagina desde codigo de py, lo mejor es usar el sistema de plantillas 
+
+#Creamos un directorio "templates" Django siempre buscara las plantillas ahi
+
+#Esta vendria a ser la funcion de vista usada para la plantilla
+def index(request):
+    ultimas_preguntas_hechas = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "ultimas_preguntas_hechas": ultimas_preguntas_hechas,
+    }
+    return HttpResponse(template.render(context, request))
+
+#Este codigo carga la platilla polls/index.html, contexto es un diccionario que mapea nombres de variables de plantilla a objetos
