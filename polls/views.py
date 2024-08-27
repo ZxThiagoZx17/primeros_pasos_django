04bfe02cd20df002d504fcbcd3844f07b4301ccf
unknown <tiagosanabria03@gmail.com>
2024-08-27 14:01:47 -0500

    Se modifica funcion Vote para que actue en el campo action del formulario de details.html

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect,Http404
 from django.template import loader
-from django.http import Http404
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -21,8 +22,35 @@ def results(request, dato_usuario_nav):
     return HttpResponse(response % dato_usuario_nav)
 
 
+# def vote(request, dato_usuario_nav):
+#     return HttpResponse("Tu voto en la pregunta es de: %s." % dato_usuario_nav)
+
+
 def vote(request, dato_usuario_nav):
-    return HttpResponse("Tu voto en la pregunta es de: %s." % dato_usuario_nav)
+    pregunta = get_object_or_404(Question, pk=dato_usuario_nav)
+    try: #request.POST es un diccionario que permite acceder a datos que han enviado mediante su nombre clave "Choice" en este caso
+         # Tambien existe  request.GET para acceder a los datos GET
+        respuesta_seleccionada = pregunta.Foranea_pregunta.get(pk=request.POST["choice"]) 
+    
+    except (KeyError, Choice.DoesNotExist): #Se da KeyError cuando no se encuentra una clave en un diccionario, muestra el formulario de nuevo con un mensaje de error
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": pregunta,
+                "error_message": "DEBES SELECCIONAR UNA OPCION",
+            },
+        )
+    
+    else:
+        respuesta_seleccionada.votes = F("votes") + 1 #F nos sirve para para acceder directamente a la base de datos y hacer la modificacion, en este caso por cada que se seleccione el campo ratio se añade un voto, posterior a ESO SE TIENE QUE GUARDAR
+        respuesta_seleccionada.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+        #Posterior a guardar el voto se redirije al usuario a la vista resultados de la encuesta
+        return HttpResponseRedirect(reverse("polls:results", args=(pregunta.id,)))
 
 #Una vista puede hacer 2 cosas, o devolver un HttpResponse con el contenido HTML que queremos ver o hacer un excepcion con Http404
 
