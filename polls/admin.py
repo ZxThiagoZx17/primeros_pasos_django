d688d8c82059ef4931db3d545017e18a8c5e1db9
unknown <tiagosanabria03@gmail.com>
2024-09-18 17:42:06 -0500

    Se añade clase QuestionAdmin Este cambio en particular anterior hace que la “Fecha de publicación” venga antes de la “Campo de Question
    q

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,13 @@ from .models import Question, Choice
 # Register your models here.
 # Para hacer que los modelos que acabo de crear en models.py sean editables en el portal de administracion de Django necesito registrarlos en admin.py asi:
 
-admin.site.register(Question)
+# admin.site.register(Question) #Con esta linea Django construye la presentacion del formulario de Question
 admin.site.register(Choice)
 
 #El panel de administracion de Django nos permitira usar operaciones CRUD con la base de datos
+
+#Ahora vamos a personalizar el formulario de administracion de Django
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    
+admin.site.register(Question, QuestionAdmin)
