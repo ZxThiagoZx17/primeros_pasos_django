62587ef3603c72985200861fefa54ac0637db895
unknown <tiagosanabria03@gmail.com>
2024-09-18 17:48:32 -0500

    Se modifica clase QuestionAdmin para dividir el formulario en conjuntos de campo y se le dio un nombre al conjunto

--- polls/admin.py
+++ polls/admin.py
@@ -9,7 +9,18 @@ admin.site.register(Choice)
 #El panel de administracion de Django nos permitira usar operaciones CRUD con la base de datos
 
 #Ahora vamos a personalizar el formulario de administracion de Django
+
+#Con esta linea definimos Este cambio en particular anterior hace que la “Fecha de publicación” venga antes de la “Campo de Question”
+
+#  class QuestionAdmin(admin.ModelAdmin):  
+#     fields = ["pub_date", "question_text"]
+
+# Con esta linea dividimos el formulario en conjuntos de campo
+# El primer elemento de la tupla "fields" hace referencia al nombre de conjunto de campo
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
-    
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Infromatiosn de fechaison", {"fields": ["pub_date"]}),
+    ]
+
 admin.site.register(Question, QuestionAdmin)
